343a1a00b1d15e728e9051a9aacb46816d6df527
rouletnorm-afk <rouletnorm@gmail.com>
2026-08-09 17:43:17 -0600

    Update version.ts
    Update version.ts

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const BUILD_VERSION = "v5.1.0";
-export const BUILD_TIMESTAMP = "2026-08-09T14:31:00Z";
+export const BUILD_VERSION = "v5.5.0";
+export const BUILD_TIMESTAMP = "2026-08-09T16:30:00Z";
